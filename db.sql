0cb245d535a28ba92e13e77863cf25888a44afcd
Billionn <chronopher@qq.com>
2016-11-29 19:51:26 +0800

    Update and rename database.sh to db.sql

---
+++ db.sql
@@ -0,0 +1,89 @@
+CREATE TABLE  `sim` (
+  `s_id` int(11) NOT NULL,
+  `sim_s_id` int(11) DEFAULT NULL,
+  `sim` int(11) DEFAULT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE  `solution` (
+  `solution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` char(48) NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
+  `memory` int(11) NOT NULL DEFAULT '0',
+  `in_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `result` smallint(6) NOT NULL DEFAULT '0',
+  `language` INT UNSIGNED NOT NULL DEFAULT '0',
+  `ip` char(15) NOT NULL,
+  `contest_id` int(11) DEFAULT NULL,
+  `valid` tinyint(4) NOT NULL DEFAULT '1',
+  `num` tinyint(4) NOT NULL DEFAULT '-1',
+  `code_length` int(11) NOT NULL DEFAULT '0',
+  `judgetime` datetime DEFAULT NULL,
+  `pass_rate` DECIMAL(2,2) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`solution_id`),
+  KEY `uid` (`user_id`),
+  KEY `pid` (`problem_id`),
+  KEY `res` (`result`),
+  KEY `cid` (`contest_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `source_code` (
+  `solution_id` int(11) NOT NULL,
+  `source` text NOT NULL,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `topic` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varbinary(60) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `top_level` int(2) NOT NULL DEFAULT '0',
+  `cid` int(11) DEFAULT NULL,
+  `pid` int(11) NOT NULL,
+  `author_id` varchar(48) NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `cid` (`cid`,`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `users` (
+  `user_id` varchar(48) NOT NULL DEFAULT '',
+  `email` varchar(100) DEFAULT NULL,
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `accesstime` datetime DEFAULT NULL,
+  `volume` int(11) NOT NULL DEFAULT '1',
+  `language` int(11) NOT NULL DEFAULT '1',
+  `password` varchar(32) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `nick` varchar(100) NOT NULL DEFAULT '',
+  `school` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `online` (
+  `hash` varchar(32) collate utf8_unicode_ci NOT NULL,
+  `ip` varchar(20) character set utf8 NOT NULL default '',
+  `ua` varchar(255) character set utf8 NOT NULL default '',
+  `refer` varchar(255) collate utf8_unicode_ci default NULL,
+  `lastmove` int(10) NOT NULL,
+  `firsttime` int(10) default NULL,
+  `uri` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`hash`),
+  UNIQUE KEY `hash` (`hash`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE  `runtimeinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `error` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `custominput` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `input_text` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
